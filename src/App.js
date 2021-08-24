46d7cc6fd6e4529163d66b2f4efc46b1ddcbd0f6
Dima <75967292+DukaDima@users.noreply.github.com>
2021-08-24 23:34:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,19 @@ class App extends Component {
       contact.name.toLowerCase().includes(normalizedFilter),
     );
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const { filter } = this.state;
     const onDeleteContact = this.deleteContact;
